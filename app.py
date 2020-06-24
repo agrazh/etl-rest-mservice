0731bb122cab7e12511388f40e613b942509e2f4
agrazh <andy.grazh@gmail.com>
2020-06-24 11:38:01 +0300

    Add endpoints

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+stores = [{
+    'name': 'Store One',
+    'items': [{'name':'Paper A4', 'price': 0.99 }]
+}]
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "root page"
+# Endpoints with GET, POST methods
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
 
-app.run(port=5000)
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/itme')
+def get_itme_in_store(name):
+    pass
+
+app.run(port=4000)
