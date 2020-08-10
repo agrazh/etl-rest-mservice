10ae567ef24387a6b1f527c686b36d579e22357b
agrazh <andy.grazh@gmail.com>
2020-08-10 13:18:03 +0200

    Add authentication and identity check

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 items = []
 
 app = Flask(__name__)
-app.secret_key = 'roi475tg#f' # temporary implementation
+app.secret_key = 'tmp_key' # temporary implementation
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity) # creates a new endpoint /auth
+
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item is not None else 404
@@ -20,12 +26,18 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+api.add_resource(Item, '/item/<string:name>')
 
 class ItemList(Resource):
     def get(self):
         return {'item': items}
 
-api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
 app.run(port=4000, debug=True)
