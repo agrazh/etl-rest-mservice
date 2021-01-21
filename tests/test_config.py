32851c8c2291eb1ff9434e41bd1736b5fca536f8
agrazh <andy.grazh@gmail.com>
2021-01-21 12:06:12 +0100

    Make of use travis-ci

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,4 +2,4 @@ import pytest
 import os.path
 
 def test_config_exists():
-    assert os.path.isfile('../app/config.yaml') == True, "Config not found."
+    assert os.path.isfile('./app/config.yaml') == True, "Config not found."
